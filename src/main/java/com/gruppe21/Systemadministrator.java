dee77b29b0a5c62ca017b8f4f20633e1cdc4b74b
Gülsen <gulsencik@live.dk>
2020-11-03 15:23:03 +0100

    Created Systemadministrator class with empty methods

---
+++ src/main/java/com/gruppe21/Systemadministrator.java
@@ -0,0 +1,15 @@
+package com.gruppe21;
+
+public class Systemadministrator {
+    public boolean opretMedarbejder(int id, int medarbejderType) {
+    }
+
+    public boolean fjernMedarbejder(int id) {
+    }
+
+    public void OpretFacilitet(int nummer, Facilitet overFacilitet) { //overFacilitet : Facilitet=null
+    }
+
+    public void flytFacilitet() {
+    }
+}
