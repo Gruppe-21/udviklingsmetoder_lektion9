bb074c2e94e9ec0fc3f547a3343b00d9b9958203
s205436 <s205436@student.dtu.dk>
2020-11-03 15:43:22 +0100

    Constructer super

--- src/main/java/com/gruppe21/Medarbejder.java
+++ src/main/java/com/gruppe21/Medarbejder.java
@@ -2,6 +2,10 @@ package com.gruppe21;
 
 public class Medarbejder extends Bruger{
 
+    public Medarbejder(int id, String navn) {
+        super(id, navn);
+    }
+
     public void setNavn(String navn){
         this.navn=navn;
     }
