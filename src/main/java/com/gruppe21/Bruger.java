ee5a4cc03d6057e4ab128d39f1aecfddd82bbee8
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2020-11-03 15:05:35 +0100

    Added Bruger

---
+++ src/main/java/com/gruppe21/Bruger.java
@@ -0,0 +1,10 @@
+package com.gruppe21;
+
+public abstract class Bruger {
+    int id;
+    String navn;
+    public Bruger(int id, String navn){
+        this.id = id;
+        this.navn = navn;
+    }
+}
